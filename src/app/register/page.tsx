965f407b605e674fd7278e8b79ff6d5e14671911
sahathiya <salamkaraya4@gmail.com>
2025-01-29 16:31:21 +0530

    registration-ui setup (#10)

---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterPage from '@/components/registerPage/RegisterPage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <RegisterPage/>
+    </div>
+  )
+}
---
+++ src/components/registerPage/QuestionPage.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export default function QuestionPage() {
+  const router = useRouter();
+
+  const handleContinue = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push(`/register/namepage/educationpage/questionpage/jobpage`);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen px-4">
+      <div className="w-full max-w-3xl p-6">
+        <h1 className="text-gray-800 font-semibold text-2xl text-center mb-6">
+          Are you looking for a job?
+        </h1>
+
+        <div className="w-full flex flex-col items-center"> {/* Centering container */}
+          <form className="space-y-4 w-full max-w-md">
+            <div className="flex items-center space-x-2 p-3 w-full border border-transparent rounded-md hover:border-purple-600 hover:ring-2 hover:ring-purple-600 transition duration-200">
+              <input
+                type="radio"
+                id="radioOption1"
+                name="radioGroup"
+                className="w-5 h-5 text-purple-600 border-gray-300"
+              />
+              <label htmlFor="radioOption1" className="text-gray-700 font-medium">
+                Yes, I'm actively looking for a new job
+              </label>
+            </div>
+
+            <div className="flex items-center space-x-2 p-3 w-full border border-transparent rounded-md hover:border-purple-600 hover:ring-2 hover:ring-purple-600 transition duration-200">
+              <input
+                type="radio"
+                id="radioOption2"
+                name="radioGroup"
+                className="w-5 h-5 text-purple-600 border-gray-300"
+              />
+              <label htmlFor="radioOption2" className="text-gray-700 font-medium">
+                Not really, but would consider the right opportunity
+              </label>
+            </div>
+
+            <div className="flex items-center space-x-2 p-3 w-full border border-transparent rounded-md hover:border-purple-600 hover:ring-2 hover:ring-purple-600 transition duration-200">
+              <input
+                type="radio"
+                id="radioOption3"
+                name="radioGroup"
+                className="w-5 h-5 text-purple-600 border-gray-300"
+              />
+              <label htmlFor="radioOption3" className="text-gray-700 font-medium">
+                No, I'm not interested in any job opportunity
+              </label>
+            </div>
+
+            <div className="w-full flex justify-center mt-4">
+              <button
+                className="w-full py-2 bg-purple-700 text-white font-semibold rounded-full hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600"
+                onClick={handleContinue}
+              >
+                Continue
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
