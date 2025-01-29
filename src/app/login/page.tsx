1e9724d59c149e0732d5831d31e013454e65078f
findlyproject <findlyproject@gmail.com>
2025-01-29 10:35:04 +0530

    i craeting login page and chaing app

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Loginpage from '@/components/loginPage/Loginpage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Loginpage/>
+    </div>
+  )
+}
+
+export default page
