8c2b3e6249912834ef42a3343831ad8e832b61d0
Aslah7070 <aslah.c7070@gmail.com>
2025-01-29 11:36:33 +0530

    set up redux folder structer and configration

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,16 +1,29 @@
+"use client"
+
 import React from 'react'
 
+
 function LandingPage() {
+
+
+
   return (
     <div>
     
-    <div className='bg-red-600'>
+    <div>
+    <div className=''>
       <h1 >
+
+    
       Find a job that suits your interest & skills.
       </h1>
       <p>Aliquam vitae turpis in diam convallis finibus in at risus. Nullam in scelerisque leo, eget sollicitudin velit bestibulum.</p>
 
       
+    </div>
+    <div>
+      
+    </div>
     </div>
 
     </div>
