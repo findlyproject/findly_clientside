91c38233cb21b6b599c85ce79217aacca955db51
findlyproject <findlyproject@gmail.com>
2025-01-30 16:15:35 +0530

    footer created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import LandingPage from "@/components/landingPage/LandingPage";
 import "../styles/globals.css"
 import Navbar from "@/components/navBar/Navbar";
 
-
+import Footer from "@/components/footer/Footer"
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <LandingPage/>
-      
+      <Footer/>
     </div>
   );
 }
