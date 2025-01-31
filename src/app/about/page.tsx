5feb304a37f6f527dd28b5994603629954b0dbd3
sahathiya <salamkaraya4@gmail.com>
2025-01-31 09:56:10 +0530

    footer

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutPage from '@/components/aboutPage/AboutPage'
+export default function page() {
+  return (
+    <div>
+      <h1>dfgh</h1>
+      <AboutPage/>
+    </div>
+  )
+}
---
+++ src/components/aboutPage/AboutPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <img src="https://i.pinimg.com/736x/3f/0c/80/3f0c80a54cea36207842cda6ea31d08b.jpg"/>
+    </div>
+  )
+}
