95476a91eca27c4ce7278438804e51be843ac577
sahathiya <salamkaraya4@gmail.com>
2025-02-04 11:16:58 +0530

    Registration UI (#41)
    * registration-ui setup
    
    * home
    
    * pushing register submit
    
    * changesin jobpage
    
    * footer
    
    * changes
    
    * changes
    
    * premiumpage
    
    * create admin login ui

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import LoginPage from '@/components/adminside/LoginPage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <LoginPage/>
+    </div>
+  )
+}
