a8e3fd32922aa677a54b56294ad01ee755852d10
sahathiya <salamkaraya4@gmail.com>
2025-02-12 14:42:14 +0530

    changes in adminlogin and userprofile

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-<<<<<<< HEAD
-    domains: ['res.cloudinary.com',"via.placeholder.com","lh3.googleusercontent.com"],
-
-=======
 
     domains: ['res.cloudinary.com',"via.placeholder.com","lh3.googleusercontent.com"], 
->>>>>>> 8f832caae7b1494e49c932d3737b877915146c15
   },
   
 };
