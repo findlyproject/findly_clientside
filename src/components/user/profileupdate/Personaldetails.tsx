fcf1f50c9d379798d2c2984125c48bf403d17b20
sahathiya <salamkaraya4@gmail.com>
2025-02-15 11:06:45 +0530

    Registration UI (#96)
    * registration-ui setup
    
    * home
    
    * pushing register submit
    
    * changesin jobpage
    
    * footer
    
    * changes
    
    * changes
    
    * premiumpage
    
    * create admin login ui
    
    * change structure
    
    * setupuser
    
    * setupuser
    
    * persist added
    
    * handleasync
    
    * setupthe userdetailes api
    
    * post changes
    
    * changes in post
    
    * posts
    
    * posts
    
    * changes in post
    
    * set up like and buttons
    
    * changes in adminlogin and userprofile
    
    * set up the editprofile
    
    * changes in register
    
    * Connectprofile (#92)
    
    * completed
    
    * replay started
    
    * faced moosu issue
    
    * resume connecting completed
    
    * replay section connecting fullty completd
    
    * edit replay and post upload is completed
    
    * edit replay and post upload is completed
    
    * emogy issue
    
    * emogy issue
    
    * removed comments
    
    * issuee on my resume
    
    * completed the ressume uploading
    
    * completed the ressume uploading
    
    * completed the ressume uploading
    
    * completed the ressume uploading
    
    * frontend
    
    * frontend
    
    * completed (#94)
    
    * changes
    
    ---------
    
    Co-authored-by: Aslah_c <aslah.c7070@gmail.com>
    Co-authored-by: rinsha4145 <rinshajouhar5181@gmail.com>

--- src/components/user/profileupdate/Personaldetails.tsx
+++ src/components/user/profileupdate/Personaldetails.tsx
@@ -70,7 +70,7 @@ function Personaldetails() {
         formData.append("folder", folder);
 
         const uploadResponse = await fetch(
-          `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+         ` https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
           {
             method: "POST",
             body: formData,
@@ -250,122 +250,6 @@ function Personaldetails() {
       </div>
     </div>
   );
-            <button onClick={() => document.getElementById("bannerUpload").click()}>
-                <FaRegEdit className="absolute top-5 right-5 text-white bg-gray-800 p-2 rounded-full cursor-pointer text-4xl" />
-            </button>
-
-            {/* Profile Image Section */}
-            <div className="absolute top-24 left-5 flex flex-col items-center">
-                <Image 
-                // src={input.profileImage ? (input?.profileImage):("/assets/profile.png")}
-                src={
-                    image?.profileImage 
-                      ? (typeof image?.profileImage === "string" 
-                          ? image?.profileImage 
-                          : URL.createObjectURL(image?.profileImage)) 
-                      : "/assets/profile.png"
-                  } 
-                    alt="profile image"
-                    width={100}
-                    height={100}
-                    className="w-32 h-32 rounded-full shadow-xl border border-gray-300 object-cover"
-                />
-                {/* Hidden Input for Profile Upload */}
-                <input
-                    type="file"
-                    accept="image/*"
-                    name='profileImage'
-                    className="hidden"
-                    id="profileUpload"
-                    onChange={handilImage}
-                />
-                <button onClick={() => document.getElementById("profileUpload").click()}>
-                    <FaRegEdit className="absolute top-20 left-24 text-white bg-gray-800 p-2 rounded-full cursor-pointer text-3xl" />
-                </button>
-            </div>
-            </div>
-
-            {/* Personal Details Form */}
-            <div className="p-6 bg-gray-100 rounded-lg shadow-lg mt-4 w-full">
-                <h2 className="text-xl font-semibold mb-4">Personal Details</h2>
-
-                <div className="grid grid-cols-2 gap-4 my-4">
-                    <div className='grid gap-3'>
-                        <label>FirstName</label>
-                        <input type="text" 
-                        name="firstName"
-                        placeholder="First Name" 
-                        value={input.firstName}
-                        onChange={handilchange}
-                        className="p-2 border rounded-md" 
-                        />
-                    </div>
-                    <div className='grid gap-3'>
-                        <label>LastName</label>
-                        <input type="text"
-                         name="lastName"
-                          placeholder="Last Name"
-                          value={input.lastName}
-                          onChange={handilchange}
-                          className="p-2 border rounded-md" 
-                          />
-                    </div>
-                </div>
-
-                <div className="grid grid-cols-2 gap-4 my-4">
-                    <div className='grid gap-3'>
-                        <label>Email</label>
-                        <input type="email"
-                         name="email"
-                          placeholder="email"
-                          value={input.email}
-                          onChange={handilchange}
-                           className="p-2 border rounded-md" 
-                           />
-                    </div>
-                    <div className='grid gap-3'>
-                        <label>Phone Number</label>
-                        <input type="text"
-                        name="phoneNumber" 
-                        placeholder="Phone Number" 
-                        value={input.phoneNumber}
-                        onChange={handilchange}
-                        className="p-2 border rounded-md" 
-                        />
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4 my-4">
-                    <div className='grid gap-3'>
-                        <label>Date of Birth</label>
-                        <input type="date"
-                        name="dateOfBirth"
-                        value={input.dateOfBirth}
-                        onChange={handilchange}
-                        className="p-2 border rounded-md" 
-                        />
-                    </div>
-
-                </div>
-                <div className='grid gap-3'>
-                    <label>About</label>
-                    <input name="about"
-                    placeholder="About Yourself"
-                    value={input.about}
-                    onChange={handilchange}
-                    className="mt-4 w-full p-2 border rounded-md"
-                    />
-                </div>
-                <div className='flex justify-end'>
-                <button
-                className='p-2 bg-green-500 rounded-lg text-xl mt-3 '
-                onClick={handilsubmit}
-                >save
-                </button>
-            </div>
-            </div>
-           
-        </div> 
-    )
 }
 
 export default Personaldetails;
