10ce2a87a01c905ffe34f344c461a3068bb04e70
sahathiya <salamkaraya4@gmail.com>
2025-02-15 10:54:27 +0530

    changes

--- src/components/homePage/leftSide/FeedIdentityModule.tsx
+++ src/components/homePage/leftSide/FeedIdentityModule.tsx
@@ -26,11 +26,7 @@ export const FeedIdentityModule = () => {
   if (!activeuser) {
     return <section className="rounded-lg border border-gray-300 min-h-[240px] bg-white flex items-center justify-center">Loading...</section>
   }
-<<<<<<< HEAD
-
-=======
   
->>>>>>> 724f9bae77726d267e8ec7f32f00ead0696c9a55
   return (
     <section className="rounded-lg border border-gray-300 min-h-[240px] bg-white">
       <div>
