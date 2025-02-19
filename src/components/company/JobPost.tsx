0144b987848119c078d9635df58639a3d76d272e
rinsha4145 <rinshajouhar5181@gmail.com>
2025-02-19 11:40:54 +0530

    job post UI

--- src/components/company/JobPost.tsx
+++ src/components/company/JobPost.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import CreatableSelect from "react-select/creatable";
 
 export const JobPost = () => {
   const [requirementInput, setRequirementInput] = useState("");
