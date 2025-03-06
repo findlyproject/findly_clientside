cc42c5738c67270897244f71e4ee61c419a21a16
rinsha4145 <rinshajouhar5181@gmail.com>
2025-03-06 22:01:54 +0530

    changes3

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -2,7 +2,7 @@ import ContactUs from "@/components/common/contactSupport/ContactUs";
 import Navbar from "@/components/navBar/Navbar";
 import React from "react";
 
-export function page() {
+export default function page() {
   return (
     <div>
       <Navbar />
