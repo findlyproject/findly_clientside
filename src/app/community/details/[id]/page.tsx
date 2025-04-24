2a8c11d6a199dfb5a613d753525814ebee66b0cc
Aslah7070 <aslah.c7070@gmail.com>
2025-04-24 13:36:47 +0530

    completed error

--- src/app/community/details/[id]/page.tsx
+++ src/app/community/details/[id]/page.tsx
@@ -2,13 +2,21 @@ import {CommunityDetails} from "@/components/common/community/CommunityDetails";
 import Navbar from "@/components/navBar/Navbar";
 import React from "react";
 
-export default function page ({ params }: { params: { id: string } }) {
+type Props = {
+  params: {
+    id: string;
+  };
+  onClose: () => never;
+};
+export default function Page({ params, onClose }: Props) {
   const { id } = params;
 
   return (
     <div>
       <Navbar />
-      <CommunityDetails params={{ id }} onClose={() => {}} />
+      <CommunityDetails params={{ id }} onClose={onClose} />
     </div>
   );
 }
+
+
