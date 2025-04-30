c3dccae8aa2c68911de4a8b83cabaf33b1de106e
Aslah_c <aslah.c7070@gmail.com>
2025-04-30 16:55:35 +0530

    Last branch (#223)
    * done
    
    * done
    
    * done
    
    * image changed
    
    * imge
    
    * completed
    
    * nextAuth fixed
    
    * next

--- src/app/(auth)/api/auth/[...nextauth]/route.ts
+++ src/app/(auth)/api/auth/[...nextauth]/route.ts
@@ -26,7 +26,8 @@ const handler = NextAuth({
       session.user = token;
       return session;
     },
-  },
+  },   
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
